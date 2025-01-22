532dc727d5456d8bede5b61ffffa27acf4925cd1
Jfranbm04 <juanfranburgos2004@gmail.com>
2025-01-22 17:03:35 +0100

    añadir_localStorage

--- 02-todo-context/src/components/TaskList.jsx
+++ 02-todo-context/src/components/TaskList.jsx
@@ -1,32 +1,39 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 
 const TaskList = () => {
-    const { task, removeTask, editTask, toggleTaskCompletion } =
-        useContext(TaskContext);
+    const { tasks, removeTask, editTask, toggleTaskCompletion } = useContext(TaskContext);
 
     return (
-        <div className="p-4 bg-gray-100 rounded-lg shadow-md ">
-            <h2 className="text-xl font-bold mb-4">Lista de Tareas</h2>
+        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold mb-4">Lista de tareas</h2>
             <ul>
-                {task.map((task) => (
-                    <li
-                        key={task.id}
-                        className="flex justify-between items-center p-2 mb-2 bg-white rounded-lg shadow-md"
-                    >
-                        <span
-                            className={`flex-1 ${task.completed ? "line-through text-gray-600" : ""
-                                }`}
+                {
+                    tasks.map((task) => (
+                        <li
+                            key={task.id}
+                            className="flex justify-between items-center p-2 mb-2 bg-white rounded-lg shadow-md"
                         >
-                            {task.title}
-                        </span>
-                        <button>Editar</button>
-                        <button>Eliminar</button>
-                    </li>
-                ))}
+                            <span className={`flex-1 mx-4 font-bold
+                                    ${task.completed ? "line-through text-gray-600" : "text-blue-500"}`}>
+                                {task.title}
+                            </span>
+                            <button
+                                className="px-3 py-1 bg-blue-700 text-white rounded mr-2
+                                hover:bg-slate-800"
+                                onClick={() => toggleTaskCompletion(task.id)}
+                            >Completar</button>
+                            <button
+                                className="px-3 py-1 bg-orange-700 text-white rounded mr-2
+                                hover:bg-slate-800"
+                                onClick={() => removeTask(task.id)}
+                            >Eliminar</button>
+                        </li>
+                    ))
+                }
             </ul>
         </div>
-    );
-};
+    )
+}
 
-export default TaskList;
+export default TaskList
--- 02-todo-context/src/context/TaskContext.jsx
+++ 02-todo-context/src/context/TaskContext.jsx
@@ -1,55 +1,71 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 /**
- * Una tarea típica ha de ser:
+ * Una tarea tipica ha de ser:
  * {
  *  id: string,
  *  title: string,
  *  completed: boolean
- *}
+ * }
  */
-// crear el contexto
+
+
+// Crear un contexto se divide en dos partes
+// 1. Crear el contexto
 export const TaskContext = createContext();
 
-// crear el proveedor (provider) del contexto
-
+// 2. Crear el proveedor (provider) del contexto
 export const TaskProvider = ({ children }) => {
-    // hooks
-    const [task, setTask] = useState(() => {
+
+    // 1. Hooks
+    const [tasks, setTasks] = useState(() => {
+        // Obtener las tareas del localStorage
         const savedTask = localStorage.getItem("task");
         return savedTask ? JSON.parse(savedTask) : [];
     });
 
-    // funciones
-    // acciones sobre una tarea:
-    // - agregar
-    // - eliminar
-    // - editar
-    // - marcar como completada
+    useEffect(() => {
+        // Guardar tasks en el localStorage
+        localStorage.setItem("task", JSON.stringify(tasks));
+    }, [tasks])
+
+    // 2. Funciones
+    //    - Añadir tarea
+    //    - Eliminar tarea
+    //    - editar tarea
+    //    - Marcar como completada
     // No olvidar que las tareas han de estar guardadas en el localStorage
+
     const addTask = (task) => {
-        setTask((prevTasks) => [...prevTasks, task]);
-    };
-
+        setTasks((prevTasks) => [...prevTasks, task]);
+    }
     const removeTask = (taskId) => {
-        setTask((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-    };
+        setTasks((prevTasks) => prevTasks.filter(task => task.id !== taskId));
+    }
+    const editTask = (taskId, task) => {    // Hacer yo
 
-    const editTask = (taskId, task) => { }; // para vosotros
-
+    }
     const toggleTaskCompletion = (taskId) => {
-        setTask((prevTasks) =>
-            prevTasks.map((task) =>
-                task.id === taskId ? { ...task, completed: !task.completed } : task
-            )
-        );
+        setTasks((prevTasks) => prevTasks.map((task) => task.id === taskId ? { ...task, completed: !task.completed } : task));
     };
 
+
+    // 3. Return -> Debe retornar algo que envuelva al children
     return (
-        <TaskContext.Provider
-            value={{ task, addTask, removeTask, editTask, toggleTaskCompletion }}
-        >
+        <TaskContext.Provider value={   // Se le pasa un objeto (task)
+            {
+                tasks: tasks,
+                addTask: addTask,
+                removeTask: removeTask,
+                editTask: editTask,
+                toggleTaskCompletion: toggleTaskCompletion,
+            }
+        }  > {/* Aquí se deben pasar las props que se desean compartir con el componente hijo */}
             {children}
         </TaskContext.Provider>
+
     );
+
 };
+
+
