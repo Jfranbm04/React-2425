e7f765240c9b436ff9cbad2d6e630c29fcc04850
Jfranbm04 <juanfranburgos2004@gmail.com>
2025-02-05 19:58:29 +0100

    06-peliculas-routuerv7

---
+++ 06-peliculas-routerv7/src/services/tmdb.js
@@ -0,0 +1,34 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_IMG_URL = import.meta.env.VITE_BASE_IMG_URL
+
+// Tamaños de las imagenes
+export const SIZE = {
+    POSTER: W500,
+    ORIGINAL: "original",
+};
+
+// Funcion para hacer fetch a la API URL, opciones
+const fetchFromApi = async (endpoint, options={}) => {
+    try{
+        // https://api.themoviedb.org/3/movie/popular?api_key=c71e9a7be9503714ea7d1b92a08e011f&language=es-ES
+        const response= await fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}&language=es-ES`);
+        const {results} = await response.json();
+        return results;
+    }catch (error){
+        throw new Error("Error fetching data");    
+    }
+};
+
+// Funcion para obtener las peliculas populares
+export const getPopularMovies = async () => {
+    return await fetchFromApi("/movie/popular");
+}
+
+export const getMovieDetail = async (id)=> {
+    return await fetchFromApi(`/movie/${id}`);
+}
+
+export const getMovieVideos = async (id) => {
+    return await fetchFromApi(`/movie/${id}/videos`);
+}
